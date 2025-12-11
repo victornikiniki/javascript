69c6281e4068ed0ed55face173e645f5e0c11c18
victornikiniki <vibhisingh1345@gmail.com>
2025-12-11 18:22:59 +1100

    variables

--- index.js
+++ index.js
@@ -7,6 +7,37 @@
 document.getElementById("myH1").textContent = `Hello`;
 document.getElementById("myP").textContent = `I like Pizza!`;
 
+let age = 27;
+let price = 10.99;
+let gpa = 3.5;
+let firstName = "Victor";
+let favoriteFood = "Udon";
+let email = "victor@gmail.com";
+let online = true;
+let forSale = false;
+let isStudent = false;
+let fullName = "Victor Nikiforov";
+
+
+console.log(age);
+console.log(price);
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is ${gpa}`);
+console.log(typeof firstName);
+console.log(firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`${firstName} likes ${favoriteFood}`);
+console.log(`${firstName}'s email is ${email}`);
+console.log(typeof online);
+console.log(`${firstName} is online: ${online}`);
+console.log(`Is this car for sale? ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
 //comment
 
 /* comments */
