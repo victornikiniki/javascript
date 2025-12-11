dc1f9421786ede84c9300477a730de1d84a439de
victornikiniki <vibhisingh1345@gmail.com>
2025-12-11 17:57:56 +1100

    Learning javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log(`Hello`);
+//console.log(`I like pizza!`)
+
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`)
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza!`;
+
+//comment
+
+/* comments */
