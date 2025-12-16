c684ea00a6a95128d8a3b690ab39f247ffb18d99
victornikiniki <vibhisingh1345@gmail.com>
2025-12-16 13:51:08 +1100

    switch

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-let age = 1;
+let day = 6;
+
+switch(day){
+    case 1: 
+        console.log("It is Monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+
+    case 3:
+        console.log("It is Wednesday");
+        break;
+
+    case 4:
+        console.log("It is Thursday");
+        break;
+
+    case 5:
+        console.log("It is Friday");
+        break;
+
+    case 6:
+        console.log("It is Saturday");
+        break;
+
+    case 7:
+        console.log("It is Sunday");
+        break;
+    
+    default: 
+        console.log(`${day} is not a day`)
+        break;
+
+}
+
+let testScore = 33;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    case testScore >= 60:
+        letterGrade = "D";
+        break;
+    default: 
+        letterGrade = "F";
+        break;
+}
+
+console.log(letterGrade);
+
+
+/* let age = 1;
 let message = age >= 18 ? "You are an adult" : "You're a minor";
 
 console.log(message);
