3e066ccf2514b288f14bd6b7bc58f537876cdddb
victornikiniki <vibhisingh1345@gmail.com>
2025-12-16 13:16:13 +1100

    working with if-else block

--- index.js
+++ index.js
@@ -1,4 +1,75 @@
-const min = 50;
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function(){
+    age = myText.value;
+    age = Number(age);
+
+    if(age>=100){
+    resultElement.textContent = `You are TOO OLD to enter this site`
+}
+    else if(age<0){
+    resultElement.textContent = `Your age can't be below 0`
+}
+    else if(age>=18){
+    resultElement.textContent = `You are old enough to enter this site`
+}
+    else{
+    resultElement.textContent = `You must be 18+ to enter this site`
+}
+}
+
+if(age >= 18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18+ to enter this site")
+}
+
+if(time<12){
+    console.log("Good morning!")
+}
+else{
+    console.log("Good afternoon!")
+}
+
+if(isStudent){
+    console.log("You are a student")
+}
+else{
+    console.log("You are not a student")
+}
+
+if(age>=16){
+    console.log("You are old enough to drive");
+
+    if(hasLicense){
+        console.log("You can drive")
+    }
+    else{
+        console.log("You need to get a license first")
+    }
+}
+else{
+    console.log("You must be 16+ to have a license")
+}
+
+if(age>=100){
+    console.log("You are too old to enter this site");
+}
+else if(age<0){
+    console.log("Your age can't be below 0")
+}
+else if(age>=18){
+    console.log("You are old enough to enter this site")
+}
+else{
+    console.log("You must be 18+ to enter this site")
+}
+
+/* const min = 50;
 const max = 100;
 
 let randomNum = Math.floor(Math.random() * (max - min)) + min;
