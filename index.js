ba9f4cf83eb0a901dc8b68b14096a28d1b51885e
victornikiniki <vibhisingh1345@gmail.com>
2025-12-16 13:30:33 +1100

    checked button functionality

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-const myText = document.getElementById("myText");
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+    if(myCheckbox.checked){
+        subResult.textContent = `You ARE subscribed`
+    }
+    else{
+        subResult.textContent = `You ARE NOT subscribed`
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with Mastercard`
+    }
+    else if(paypalBtn.checked){
+        paymentResult.textContent = `You are paying with Paypal`
+    }
+    else{
+        paymentResult.textContent = `You must select a payment method`
+    }
+}
+
+
+/* const myText = document.getElementById("myText");
 const mySubmit = document.getElementById("mySubmit");
 const resultElement = document.getElementById("resultElement");
 let age;
