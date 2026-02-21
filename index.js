a3696679db55da09df26ce93998d94206d2ebf1c
victornikiniki <vibhisingh1345@gmail.com>
2026-02-21 14:22:52 +1100

    for loop, continue, break

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+ for (let i = 1; i <= 20; i++){
+    if (i == 13){
+        break; 
+    }
+    else{
+        console.log(i);
+    }
+}
+
+/* for (let i = 10; i >= 0; i--){
+    console.log(i)
+}
+console.log("Happy new year!")
+
+
 /* Check login credentials */
 
 /* let isLoggedin = false; 
